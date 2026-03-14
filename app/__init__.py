e2a9311bdeaa34bb335ade314494cbe30e727998
Wjm Piek <wjm@martinsdirect.com>
2026-03-14 09:42:33 +0200

    update dashboard

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,19 @@
-from .factory import create_app
+from flask import Flask
 
-__all__ = ["create_app"]
+def create_app():
+    app = Flask(__name__)
+
+    # import and register blueprints here
+    from app.routes.api import api_bp
+    from app.routes.main import main_bp
+    from app.routes.auth import auth_bp
+    from app.routes.admin import admin_bp
+    from app.routes.web_upload import upload_bp
+
+    app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(admin_bp)
+    app.register_blueprint(api_bp)
+    app.register_blueprint(upload_bp)
+
+    return app

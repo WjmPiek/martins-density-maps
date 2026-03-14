f139ca7760c438b739b25565c952e98136c8d880
Wjm Piek <wjm@martinsdirect.com>
2026-03-14 12:00:49 +0200

    update dashboard

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,22 @@
 from flask import Flask
+from .extensions import db, login_manager
 
 def create_app():
     app = Flask(__name__)
 
-    from app.routes.api import api_bp
+    app.config.from_object("config.Config")
+    app.config["SECRET_KEY"] = "your-secret-key"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "your-db-uri"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = "auth.login"
+
     from app.routes.main import main_bp
     from app.routes.auth import auth_bp
     from app.routes.admin import admin_bp
+    from app.routes.api import api_bp
     from app.routes.web_upload import upload_bp
 
     app.register_blueprint(main_bp)
--- app/extensions.py
+++ app/extensions.py
@@ -1,8 +1,13 @@
-from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 login_manager.login_view = "auth.login"
 login_manager.login_message_category = "warning"
+
+from app.models import User
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))

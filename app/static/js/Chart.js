96c1837c15f18565c70da8a2e4326416024879b1
Wjm Piek <wjm@martinsdirect.com>
2026-03-16 18:37:14 +0200

    update dashboard

--- app/static/js/Chart.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  if (window.Chart) return;
-  const script = document.createElement('script');
-  script.src = 'https://cdn.jsdelivr.net/npm/chart.js';
-  script.async = true;
-  document.head.appendChild(script);
-})();
